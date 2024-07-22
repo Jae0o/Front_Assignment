17a81fbb3328ed17bf07733165adf02ae5587357
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-22 15:35:13 +0900

    🎨style: GlobalStyles 생성

---
+++ src/styles/GlobalStyles/GlobalStyles.tsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+  html {  
+    width: 100dvw;
+    height: 100dvh;
+    font-size: 62.5%;
+    background-color: red;
+  }
+
+
+`;
+
+export default GlobalStyles;
