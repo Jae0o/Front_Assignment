1ada563f5938c8f3c0f85fceddc38e916dbb50bf
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-24 00:24:36 +0900

    🚜change: Type 수정 & TaskItemListType 추가

--- src/types/Task/Task.ts
+++ src/types/Task/Task.ts
@@ -1,7 +1,14 @@
-export type TaskStatusType = "No Status" | "ToDo" | "In Progress" | "Done";
+export type TaskStatusType = "NO_STATUS" | "TODO" | "IN_PROGRESS" | "DONE";
 
 export interface TaskItemType {
   id: string;
   content: string;
   status: TaskStatusType;
+}
+
+export interface TaskItemListType {
+  NO_STATUS: TaskItemType[];
+  TODO: TaskItemType[];
+  IN_PROGRESS: TaskItemType[];
+  DONE: TaskItemType[];
 }
