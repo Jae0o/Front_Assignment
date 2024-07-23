ea7c28f2998c9888f24d6d1e4957a1c4a09bfafe
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-24 00:25:48 +0900

    💎feat: DragEnd 로직 List 별 기능 추가

--- src/components/TaskBoard/TaskBoard.tsx
+++ src/components/TaskBoard/TaskBoard.tsx
@@ -5,13 +5,13 @@ import * as S from "./TaskBoard.styles";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
 import { TaskList } from "./components";
-import { TaskItemType, TaskStatusType } from "@/types";
+import { TaskItemType, TaskItemListType, TaskStatusType } from "@/types";
 
 export const TASK_STATUS: TaskStatusType[] = [
-  "No Status",
-  "ToDo",
-  "In Progress",
-  "Done",
+  "NO_STATUS",
+  "TODO",
+  "IN_PROGRESS",
+  "DONE",
 ];
 
 const TaskBoard = () => {
@@ -19,34 +19,68 @@ const TaskBoard = () => {
     Array.from({ length: count }, (v, k) => k).map((k) => ({
       id: `item-${k}`,
       content: `item ${k}`,
-      status: "No Status",
+      status: "NO_STATUS",
     }));
 
-  const [items, setItems] = useState<TaskItemType[]>(getItems(10));
+  const [items, setItems] = useState<TaskItemListType>({
+    NO_STATUS: getItems(10),
+    TODO: [],
+    IN_PROGRESS: [],
+    DONE: [],
+  });
 
-  const reorder = (
-    list: TaskItemType[],
-    startIndex: number,
-    endIndex: number
-  ) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-    return result;
+  const reorder = ({
+    list,
+    sourceId,
+    destinationId,
+    startIndex,
+    endIndex,
+  }: {
+    list: TaskItemListType;
+    sourceId: TaskStatusType;
+    destinationId: TaskStatusType;
+    startIndex: number;
+    endIndex: number;
+  }) => {
+    const [removed] = list[sourceId].splice(startIndex, 1);
+    list[destinationId].splice(endIndex, 0, removed);
+  };
+
+  const getCheckedStatusType = (status: string): TaskStatusType | false => {
+    if (
+      status !== "NO_STATUS" &&
+      status !== "TODO" &&
+      status !== "IN_PROGRESS" &&
+      status !== "DONE"
+    ) {
+      return false;
+    }
+
+    return status;
   };
 
   const onDragEnd = useCallback(
-    (result: DropResult) => {
-      if (!result.destination) {
+    ({ destination, source }: DropResult) => {
+      if (!destination) {
         return;
       }
-      console.log(result);
 
-      const newItems = reorder(
-        items,
-        result.source.index,
-        result.destination.index
-      );
+      const destinationId = getCheckedStatusType(destination.droppableId);
+      const sourceId = getCheckedStatusType(source.droppableId);
+
+      if (!destinationId || !sourceId) {
+        return;
+      }
+
+      const newItems: TaskItemListType = JSON.parse(JSON.stringify(items));
+
+      reorder({
+        list: newItems,
+        sourceId: sourceId,
+        destinationId: destinationId,
+        startIndex: source.index,
+        endIndex: destination.index,
+      });
 
       setItems(newItems);
     },
@@ -57,11 +91,7 @@ const TaskBoard = () => {
     <S.TaskBoardLayout>
       <DragDropContext onDragEnd={onDragEnd}>
         {TASK_STATUS.map((status) => (
-          <TaskList
-            key={status}
-            items={items.filter((item) => status === item.status)}
-            status={status}
-          />
+          <TaskList key={status} items={items[status]} status={status} />
         ))}
       </DragDropContext>
     </S.TaskBoardLayout>
